1b7a6f4c83c8e634aed9692ff9a454238ba886b1
Yikoo <tangxinzhang@126.com>
2023-10-28 21:16:05 +0800

    test

--- src/CaptchaServiceProvider.php
+++ src/CaptchaServiceProvider.php
@@ -4,13 +4,20 @@ use Illuminate\Support\ServiceProvider;
 class CaptchaServiceProvider extends ServiceProvider
 {
     public function boot() : void {
-
+        // Publish configuration files
+        $this->publishes([
+            __DIR__ . '/../config/captcha.php' => config_path('captcha.php')
+        ], 'config');
     }
     public function register() : void {
         // Merge configs
-//        $this->mergeConfigFrom(
-//            __DIR__ . '/../config/captcha.php',
-//            'captcha'
-//        );
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/captcha.php',
+            'captcha'
+        );
+        // Bind captcha
+        $this->app->bind('captcha', function ($app) {
+            return new Captcha( $app['config']);
+        });
     }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,31 @@
 <?php
-//
+use Illuminate\Container\Container;
+if (! function_exists('app')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param  string|null  $abstract
+     * @param  array  $parameters
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Foundation\Application|mixed
+     */
+    function app($abstract = null, array $parameters = [])
+    {
+        if (is_null($abstract)) {
+            return Container::getInstance();
+        }
+
+        return Container::getInstance()->make($abstract, $parameters);
+    }
+}
+if (! function_exists('config_path')) {
+    /**
+     * Get the configuration path.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->configPath($path);
+    }
+}
